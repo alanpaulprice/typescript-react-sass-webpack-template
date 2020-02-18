bcd3f4185fbd997891801888a992142d9f3aec6c
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2020-02-18 20:09:21 +0000

    flesh out installation and configuration

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PrettierPlugin = require("prettier-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    "mode": "development",
+    "entry": __dirname + "/src/scripts/index.tsx",
+    "output": {
+        "path": __dirname + '/dist',
+        "filename": "js/[name].[chunkhash:8].bundle.js"
+    },
+    "module": {
+        "rules": [
+            {
+                "test": /\.tsx?$/,
+                "exclude": /node_modules/,
+                "use": {
+                    "loader": "ts-loader",
+                    "options": {
+                        "transpileOnly": true
+                    }
+                }
+            },
+            {
+                "test": /\.scss$/,
+                "use": [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    "plugins": [
+        new HtmlWebpackPlugin(),
+        new PrettierPlugin(),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(
+            {
+                filename: "[name]-[contenthash:8].css"
+            }
+        )
+    ]
+}
